815734b6ad5e52c8dd5c053dd468038b3f173655
Jynx <thekillercraftlp@gmail.com>
2023-05-17 17:58:45 +0200

    Moved .toLowerCase from fetch to name var

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,13 +2,13 @@
 export const actions = {
     default: async ({request}) => {
         let data = await request.formData();
-        let name = data.get('pokemonName');
+        let name = data.get('pokemonName').toLowerCase();
 
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const pokemonData = await response.json();
 
         pokemonData.name = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.substring(1);
-        
+
         return pokemonData;
     }
 };
