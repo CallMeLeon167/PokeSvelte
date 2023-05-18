9471b1e6d306cc9da0a6512992929e2f6350b106
Leon Schmidt <leon18058r@gmail.com>
2023-05-18 14:48:09 +0200

    input changed + pokemon suggestions on input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,16 @@
   import AiOutlineSearch from 'svelte-icons-pack/ai/AiOutlineSearch';
 
   let pokemonData, pokemonSprite;
+  let inputText = "";
+  let suggestions = [];
+
+  async function autocompletePokemon() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+    const data = await response.json();
+    const pokemonNames = data.results.map(pokemon => pokemon.name);
+
+    suggestions = pokemonNames.filter(name => name.startsWith(inputText.toLowerCase()));
+  }
 
 </script>
   
@@ -17,9 +27,17 @@
       pokemonSprite = pokemonData['sprites']['other']['official-artwork']['front_default'];
     }
   }}>
-    <input type="text" name="pokemonName" placeholder="eg. Charmander" autocomplete="off">
-    <button><Icon src={AiOutlineSearch}/></button>
+    <input type="text" name="pokemonName" placeholder="eg. Charmander" autocomplete="off" bind:value={inputText} on:input={autocompletePokemon} list="pokemonSuggestions">
+    
+    <datalist id="pokemonSuggestions">
+      {#each suggestions as suggestion}
+        <option value="{suggestion}">
+      {/each}
+    </datalist>
+
+    <button id="searchPokemon"><Icon src={AiOutlineSearch}/></button>
   </form>
+
 
   {#if pokemonData}
   <div id="pokemon-content">
@@ -42,16 +60,32 @@
     border-radius: 15px;
     filter: drop-shadow(0 3px 3px black);
     padding: 0 10px;
+    border: 2px solid #FFCB05;
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+    transition: background-color 0.3s ease;
+    transition: transform 0.3s ease;
+    transform: translateY(0);
+  }
+
+  form:hover {
+    background-color: #cccbcb;
+    transform: translateY(-3px);
   }
   
   form > input {
+    display: inline-block;
+    padding: 10px;
+    border-radius: 5px;
     background-color: transparent;
-    text-align: center;
-    outline: none;
+    color: #333;
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-size: 16px;
     border: none;
-    padding: 0 5px;
-    font-size: 1.5rem;
-    border-radius: 20px;
+    outline: none;
   }
 
   form > button {
@@ -61,11 +95,15 @@
     font-size: 1.2rem;
     height: 100%;
     aspect-ratio: 1;
+    border-radius: 5px;
+    transition: transform 0.3s ease;
   }
 
   form > button:hover {
     background-color: rgba(0,0,0,0.2);
+    transform: scale(1.08);
   }
+
   #pokemon-content {
     display: flex;
     flex-direction: column;
@@ -80,6 +118,10 @@
 
   #pokemon-name {
     font-size: 3rem;
+  }
+
+  #searchPokemon{
+    display: flex;
   }
 
   #pokemon-sprite {
